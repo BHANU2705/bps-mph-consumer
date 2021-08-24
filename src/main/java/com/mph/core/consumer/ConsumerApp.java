7120ef11b232560a118e43b8c6b755548622475f
bhanu <bhanu@DESKTOP-B8ADCR0>
2021-08-24 10:35:36 +0530

    adding topic

--- src/main/java/com/mph/core/consumer/ConsumerApp.java
+++ src/main/java/com/mph/core/consumer/ConsumerApp.java
@@ -1,6 +1,7 @@
 package com.mph.core.consumer;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -13,15 +14,19 @@ public class ConsumerApp {
 	public static void main(String[] args) {
 		Consumer<Long, String> consumerForCigna = ConsumerFactory.createConsumer(Payer.CIGNA);
 		Consumer<Long, String> consumerForAetna = ConsumerFactory.createConsumer(Payer.AETNA);
-
+		ConsumerApp app = new ConsumerApp();
+		app.consume(consumerForAetna, 3);
+		app.consume(consumerForCigna, 6);
+	}
+	
+	private void consume(Consumer<Long, String> consumer, int timeTakenSeconds) {
 		int noMessageFound = 0;
 
 		while (true) {
-			ConsumerRecords<Long, String> cignaConsumerRecords = consumerForCigna.poll(Duration.ofMillis(1000));
-			ConsumerRecords<Long, String> aetnaConsumerRecords = consumerForAetna.poll(Duration.ofMillis(1000));
+			ConsumerRecords<Long, String> consumerRecords = consumer.poll(Duration.ofMillis(1000));
 			// 1000 is the time in milliseconds consumer will wait if no record is found at
 			// broker.
-			if (cignaConsumerRecords.count() == 0) {
+			if (consumerRecords.count() == 0) {
 				noMessageFound++;
 				if (noMessageFound > IKafkaConstants.MAX_NO_MESSAGE_FOUND_COUNT)
 					// If no message found count is reached to threshold exit loop.
@@ -31,18 +36,22 @@ public class ConsumerApp {
 			}
 
 			// print each record.
-			cignaConsumerRecords.forEach(record -> {
-				System.out.println("[Consumer] Record Key " + record.key()
+			consumerRecords.forEach(record -> {
+				try {
+					TimeUnit.SECONDS.sleep(timeTakenSeconds);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("[Consumer : "+ record.topic() +"] Record Key " + record.key()
 				+ " Record value " + record.value()
 				+ " Record partition " + record.partition()
 				+ " Record offset " + record.offset());
 			});
-
+			
 			// commits the offset of record to broker.
-			consumerForCigna.commitAsync();
+			consumer.commitAsync();
 		}
-		consumerForCigna.close();
-
+		consumer.close();
 	}
 
 }
